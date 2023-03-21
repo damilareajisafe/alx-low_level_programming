182ced590ea97dbae46f99ee11067b79d575e3c9
damilareajisafe <dammyajisafe@gmail.com>
2023-03-21 14:16:44 -0700

    Add: 1-alphabet.c to 0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet
+ *
+ * Return - void
+ */
+void print_alphabet(void)
+{
+	for (int i = 0; i < 26; i++)
+		_putchar('a' + i);
+
+	_putchar('\n');
+}
