e421cdd1f03a217d390fa6b4f068da865212d5d0
damilareajisafe <dammyajisafe@gmail.com>
2023-03-21 14:42:08 -0700

    Add: 2-print_alphabet_x10.c to 0x02-functions_nested_loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,8 +7,12 @@
  */
 void print_alphabet_x10(void)
 {
-	int i;
+	int i, j;
 
 	for (i = 0; i < 10; i++)
-		print_alphabet();
+	{
+		for (j = 0; j < 26; j++)
+			_putchar('a' + j);
+		_putchar('\n');
+	}
 }
