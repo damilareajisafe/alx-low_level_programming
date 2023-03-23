83ffaa1adf435f317908c96e38e8a44f5431fdd5
damilareajisafe <dammyajisafe@gmail.com>
2023-03-23 15:10:54 -0700

    Add: 9-fizz_buzz.c to 0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,15 +15,16 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else if (i % 3 != 0 && i % 5 == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz ");
-		else if (i == 100)
-			printf("%d", i);
+			printf("FizzBuzz");
 		else
-			printf("%d ", i);
+			printf("%d", i);
+
+		if (i != 100)
+			printf(" ");
 	}
 	printf("\n");
 
