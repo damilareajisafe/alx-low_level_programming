2c1c8152ad8e3c2a53fc52df9ce23f7420ed56cd
damilareajisafe <dammyajisafe@gmail.com>
2023-03-27 14:54:28 -0700

    Add: 4-print_rev.c, 4-main.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: string
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+/*	int length = 0; */
+
+	while (s[i] != '\0')
+	{
+/*		length += 1; */
+		i++;
+	}
+
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
