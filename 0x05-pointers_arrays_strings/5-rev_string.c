8b0a6cfbe9c5bb64713b1102b4c6cd6ee57de1b0
damilareajisafe <dammyajisafe@gmail.com>
2023-03-27 16:00:34 -0700

    Add: 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: the string
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+}
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string,
+ * starting with the first character, followed by a new line.
+ * @str: string
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; *(str + i) != '\0'; i++)
+		if (i % 2 != 0)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(*(str + i));
+		}
+	_putchar('\n');
+}
