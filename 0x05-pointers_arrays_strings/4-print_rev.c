f9097f65fca8107224a789d416423a11fb690949
damilareajisafe <dammyajisafe@gmail.com>
2023-03-28 13:50:42 -0700

    Add: 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,13 +10,10 @@ void print_rev(char *s)
 {
 	int i = 0, length = 0;
 
-	for (i = 0; *(s + i) != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 		length += 1;
 
-	while (length - 1 >= 0)
-	{
-		_putchar(*(s + length));
-		length -= 1;
-	}
+	for (i = length - 1; i >= 0; i--)
+		_putchar(s[i]);
 	_putchar('\n');
 }
