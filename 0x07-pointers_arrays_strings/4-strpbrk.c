ee5ef4e717580bfbbbbac162008c3614ff722926
damilareajisafe <dammyajisafe@gmail.com>
2023-04-04 14:28:16 -0700

    Add: 4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: the string to be searched
+ * @accept: the bytes to be searched for
+ * Return: pointer to the byte in s that matches one of the bytes in accept
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+	char *p = s;
+
+	for (i = 0; *(p + i) != '\0'; i++)
+	{
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+			if (*(p + i) == *(accept + j))
+				return (p + i);
+		}
+	}
+	return (p + i);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,3 +5,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 void print_diagsums(int *a, int size);
 void print_chessboard(char (*a)[8]);
+char *_strpbrk(char *s, char *accept);
