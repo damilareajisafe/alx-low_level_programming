48af43daaccc662793be251dcbabdae487fc7c2e
damilareajisafe <dammyajisafe@gmail.com>
2023-04-04 15:05:22 -0700

    Add: 5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: the string to be searched
+ * @needle: the substring to be located
+ * Return: pointer to the beginning of the located substring or NULL
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; *(haystack + i) != '\0'; i++)
+	{
+		if (*needle == *(haystack + i))
+		{
+			for (j = 0; *(needle + j) != '\0'; j++)
+			{
+				if (*(needle + j) != *(haystack + i + j))
+					return ('\0');
+			}
+			break;
+		}
+	}
+	return (haystack + i);
+}
