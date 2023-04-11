78758c1f6458f52ff93e6da1a499fbb46511f5f8
damilareajisafe <dammyajisafe@gmail.com>
2023-04-11 14:21:23 -0700

    Add: 0-create_array.c, readme file

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars and initializes it with @c
+ * @c: char to be initialized
+ * @size: size of array to be created
+ * Return: a pointer to the array or NULL if it fails
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *p = malloc(size * sizeof(char));
+
+	for (i = 0; i < size; i++)
+		p[i] = c;
+
+	return (p);
+}
