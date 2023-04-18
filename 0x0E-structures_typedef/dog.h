85e41dc999ea0c28215c945c86b9319ce088d38f
damilareajisafe <dammyajisafe@gmail.com>
2023-04-18 11:46:28 -0700

    dog.h fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,13 +12,14 @@ typedef struct dog
 	float age;
 	char *owner;
 } dog_t;
-
-/* struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-} dog_t;*/
+/*
+*struct dog
+*{
+*	char *name;
+*	float age;
+*	char *owner;
+*} dog_t;
+*/
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
