b8317343a6d9546a4879e986ec3179b95264fd14
damilareajisafe <dammyajisafe@gmail.com>
2023-04-30 10:58:01 -0700

    fixed 2-add_nodeint.c to conform to betty

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -5,6 +5,7 @@
  * add_nodeint - adds a new node at the beginning of a listint_t list
  * @head: address to the head pointer
  * @n: integer
+ * Return: returns the address of the new element or NULL if it failed
  */
 
 listint_t *add_nodeint(listint_t **head, const int n)
