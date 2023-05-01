c70deefc504b3cdacbb889620086153530b7bb59
damilareajisafe <dammyajisafe@gmail.com>
2023-05-01 12:56:56 -0700

    updated 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,14 +9,12 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *temp = NULL, *h = *head;
+	listint_t *temp;
 
-	while (h != NULL)
+	while (*head != NULL)
 	{
-		temp = h->next;
-		free(h);
-		h = temp;
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
 	}
-
-	*head = NULL;
 }
