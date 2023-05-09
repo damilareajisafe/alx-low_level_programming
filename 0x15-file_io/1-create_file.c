d00d6c059b92195313bce5bb91b77c4477568805
damilareajisafe <dammyajisafe@gmail.com>
2023-05-09 12:43:57 -0700

    Add: 1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <unistd.h>
+#include <fcntl.h>
+
+/**
+ * create_file - creates a file
+ * @filename: the name of the file to be created
+ * @text_content: a string to be written to the file
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	int length = 0, count, i;
+
+	for (i = 0; text_content[i] != '\0'; i++)
+		length++;
+
+	count = write(fd, text_content, length);
+	close(fd);
+
+	if (fd == -1 || count == -1)
+		return (-1);
+
+	return (1);
+}
